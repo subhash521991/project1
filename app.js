7c6bfca3597dc55b0a641a62c93339ad869dc0a3
himanshu <himanshuchauhan696@gmail.com>
2023-08-10 00:27:44 +0530

    jhfjg

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 require('dotenv').config()
+const express = require('express');
 const bodyParser = require("body-parser");
 const route = require('./routes/route.js');
 const mongoose = require("mongoose");
