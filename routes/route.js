f3a040744f6816edd83c9f6e95a0d906bb7c1492
Subhash Mourya <subhash@euronics.com>
2023-08-11 10:44:38 +0530

    Add Final Code For Project1

--- routes/route.js
+++ routes/route.js
@@ -5,19 +5,19 @@ const middlewares = require('../middleware/auth.js');
 
 const authorController = require('../controllers/authorController.js');
 const blogController = require('../controllers/blogController.js');
-const getController = require('../controllers/getController.js');
-const deleteController = require('../controllers/deleteController.js');
-const updateController = require('../controllers/updateController.js');
-const loginController = require('../controllers/loginController.js');
+//const getController = require('../controllers/getController.js');
+//const deleteController = require('../controllers/deleteController.js');
+//const updateController = require('../controllers/updateController.js');
+//const loginController = require('../controllers/loginController.js');
 
 router.post('/authors', authorController.createAuthor);
-router.post('/createBlog', middlewares.authentication, blogController.createBlog);
-router.post('/userLogin', loginController.login);
+router.post('/createBlog', middlewares.authentication, blogController.createBlog); 
+//router.post('/userLogin', loginController.login);
 
-router.get('/blogs/:authorId', middlewares.authentication, getController.getBlog);
-router.get('/blogs', middlewares.authentication, getController.getBlog);
-router.delete('/deleteblog/:blogId', deleteController.deleteBlog);
-router.delete('/deleteblog', deleteController.deleteBlog);
-router.put('/updateBlog/:blogId', updateController.updateBlog); 
-router.put('/updateBlog', updateController.updateBlog);
+//router.get('/blogs/:authorId', middlewares.authentication, getController.getBlog);
+//router.get('/blogs', middlewares.authentication, getController.getBlog);
+//router.delete('/deleteblog/:blogId', deleteController.deleteBlog);
+//router.delete('/deleteblog', deleteController.deleteBlog);
+//router.put('/updateBlog/:blogId', updateController.updateBlog); 
+//router.put('/updateBlog', updateController.updateBlog);
 module.exports = router;  
